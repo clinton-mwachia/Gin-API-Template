2516c12d69f257e5c6cb00def5b82f51f88632a6
Clinton Moshe <clintonmwachia9@gmail.com>
2025-02-01 08:41:15 +0300

    ADDED: required libraries

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"gin-api/routes"
 	"gin-api/utils"
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -15,4 +18,17 @@ func main() {
 
 	// Connect to MongoDB
 	utils.ConnectDB()
+
+	// Initialize Gin router
+	router := gin.Default()
+
+	// Setup routes
+	routes.SetupRoutes(router)
+
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(router.Run(":" + port))
 }
