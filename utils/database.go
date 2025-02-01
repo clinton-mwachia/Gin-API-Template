ba99167b354457b9a20edd7cf1db0b78652ba666
Clinton Moshe <clintonmwachia9@gmail.com>
2025-02-01 08:15:11 +0300

    ADDED: database setup

---
+++ utils/database.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB() {
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB")
+	}
+
+	DB = client.Database(os.Getenv("DB_NAME"))
+	log.Println("Connected to MongoDB")
+}
+
+func GetCollection(name string) *mongo.Collection {
+	return DB.Collection(name)
+}
