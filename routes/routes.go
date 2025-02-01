868539ba71f45e78a9d6eae10231875d73e8cce6
Clinton Moshe <clintonmwachia9@gmail.com>
2025-02-01 10:52:37 +0300

    ADDED: user and todos routes

--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,20 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	api := router.Group("/v1/api")
 	{
+		// users routes
 		api.POST("/login", handlers.Login)
+		api.POST("/register", handlers.Register)
+		api.GET("/user/:id", handlers.GetUserByID)
+		api.GET("/users", handlers.GetUsers)
+		api.PUT("/user/:id", handlers.UpdateUser)
+		api.DELETE("/user/:id", handlers.DeleteUser)
+
+		// tasks routes
+		api.POST("/task", handlers.CreateTask)
+		api.GET("/tasks", handlers.GetTasks)
+		api.GET("/task/:id", handlers.GetTaskByID)
+		api.PUT("/task/:id", handlers.UpdateTask)
+		api.DELETE("/task/:id", handlers.DeleteTask)
+		api.GET("/tasks/paginated", handlers.GetPaginatedTasks)
 	}
 }
